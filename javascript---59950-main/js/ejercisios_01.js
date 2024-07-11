0b7557dbf95f3f7b97fb615d1092c303f2114ad4
nicolas <nicosebastiani90@gmail.com>
2024-07-11 20:38:03 -0300

    fin ejercisios

---
+++ javascript---59950-main/js/ejercisios_01.js
@@ -0,0 +1,87 @@
+/// ejercisios 01 
+
+/// 1 a 11 
+
+// console.log('Ejercisios de 1 a 11')
+// console.log('hola, mundo');
+// console.log('nicolas');
+// console.log(;)
+// console.log(5+3)
+// console.log(10-7)
+// let myName = 'nico'
+// console.log(myName)
+// let age = 33
+// console.log(age)
+// console.log(8*5)
+// console.log(20/4)
+// let a = 2
+// let b = 5
+// console.log(a + b)
+// let lastName = "sebastiani"
+// console.log(myName + " " + lastName)
+
+//// 12 a 21
+
+// console.log('Ejercisios 12 a 21')
+// let userName = prompt ('Ingresa tu nombre: ')
+// console.log(userName)
+// let userAge = prompt('Insert your age: ')
+// console.log('Your age is: ' + userAge)
+// let a = prompt('Insert a number: ')
+// let b = prompt('Insert another number: ')
+// console.log(parseInt(a) + parseInt(b))
+// let favColor = prompt('Insert your favourite color: ')
+// console.log(favColor)
+// let frase = prompt('Insert a phrase: ')
+// console.log(frase)
+// let favSport = prompt('Insert your favourite sport: ')
+// console.log(favSport)
+
+//// 22 a 31
+
+// alert('Welcome')
+// let food = prompt('Tell me your favourite food: ')
+// alert(food)
+// alert('goof morning')
+// alert('good afternoon')
+// alert('good night')
+// let season = prompt('Tell me your favourite season: ')
+// alert(season)
+
+//// 32 a 50
+
+// let userName = prompt('Tell me your name: ')
+// let userLastName = prompt('Tell me your last name: ')
+// alert(userName + " " + userLastName)
+
+// let userAge = prompt('Insert your age: ')
+// let year = 2023
+// let born = year - userAge
+// if (userAge >= 18) {
+//     alert('You have born in ' + born + ' and you are an adult')
+// } else {
+//     alert('You have born in ' + born + ' and you are not an adult')
+// }
+
+// let yearBorn = prompt('Insert your year of born: ')
+// let year = 2024
+// let age = year - parseInt(yearBorn)
+// console.log('You are ' + age + ' years old.')
+
+// let month = prompt('Insert the mouth when you born: ')
+// switch(month) {
+//     case 'january':
+//         console.log('Your sign is piscis.')
+//     case 'february':
+//         console.log('Your sign is libra.')
+//     case 'march':
+//         console.log('Your sign is virgo.')
+//     case 'april':
+//         console.log('Your sign is acuario.')
+//     case 'june':
+//         console.log('Your sign is scorpio.')
+//     case 'july':
+//         console.log('Your sign is capricornio.')
+//     case 'august':
+//         console.log('Your sign is leo.')
+// }
