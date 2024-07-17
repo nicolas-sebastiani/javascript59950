f38416b4c8c3c1576757f93588beff0e4fd27bde
nicolas <nicosebastiani90@gmail.com>
2024-07-16 22:06:28 -0300

    solucion ejercisios

---
+++ javascript---59950-main/js/ejercisios_02.js
@@ -0,0 +1,79 @@
+/// ejercisios 02 
+
+/// 1 a 3
+
+// let nombre = "Nico"
+// const PI = 3.14
+// let edad = 18
+// let esMayorDeEdad = true
+// if (edad >= 18) {
+//         esMayorDeEdad = true
+//         console.log('Es mayor de edad.')
+//     }
+// else {
+//         esMayorDeEdad = false
+//         console.log('No lo es.')
+// }
+
+/// 4 a 6
+
+// let edadUsuario = prompt('Ingrese su edad: ')
+// alert('¡Hola!')
+// console.log('La edad ingresasa es: ' + edadUsuario)
+
+// 7 a 9
+
+// let number = parseInt(prompt('Ingrese un numero: '))
+// if (number > 0) {
+//     console.log('El numero ingresado es positivo.')
+// }
+// else if (number == 0){
+//     console.log('El numero ingresado es cero.')
+// }
+// else {
+//     console.log('El numero ingresado es negativo.')
+// }
+
+// if (number % 2 == 0) {
+//     console.log(`el numero ${number} es par.`)
+// }
+// else {
+//     console.log(`el numero ${number} es impar`)
+// }
+
+// 10 a 12
+
+// let numero = parseInt(prompt("Ingrese un numero: "))
+// if (numero % 2 == 0 && numero % 3 == 0) {
+//     console.log(`El numero ${numero} es divisible tanto por 2 como por 3.`)
+// }
+// else {
+//     console.log(`El numero ${numero} no es divisible ni por 2 ni por 3.`)
+// }
+
+// if (numero > 0 && numero < 100) {
+//     console.log(`El numero ${numero} es mayor que 0 y menor que 100.`)
+// }
+// else {
+//     console.log(`El numero ${numero} no esta entre 0 y 100.`)
+// }
+
+// if (numero > 10 || numero < -10) {
+//     console.log(`El numero ${numero} es mayor que 10 o menor que -10.`)
+// }
+// else {
+//     console.log(`El numero ${numero} NO es mayor que 10 o menor que -10.`)
+// }
+
+// 13 a 
+
+// let userNumber = parseInt(prompt('Ingrese un numero: '))
+
+// if (userNumber % 2 != 0) {
+//     console.log(`El numero ${userNumber} SI es un numero primo.`)
+// }
+// else {
+//     console.log(`El numero ${userNumber} NO es un numero primo.`)
+// }
+
+let hora = 7
