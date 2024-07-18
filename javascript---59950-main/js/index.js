9773553bfdb83b23aceb2c3d64ce8f40a3108440
nicolas <nicosebastiani90@gmail.com>
2024-07-18 15:32:18 -0300

    agregan ejercisios

--- javascript---59950-main/js/index.js
+++ javascript---59950-main/js/index.js
@@ -1,122 +1,102 @@
-// if(condicion){
-//     lo que queremos que se ejecute.!
+// Ciclos e iteraciones
+// Actualizacion:
+// let i = 0;
+// console.log(i)
+// i = i + 1;
+// console.log(i)
+// i = i + 1;
+// console.log(i)
+// i = i + 1; // i++
+// console.log(i)
+// i++
+// console.log("i Actualizado con i++ ", i)
+
+// Ciclo for
+// for(desde, hasta, actualizacion ) {
+//     lo que quiero que se ejecute;
+// }
+// let i = 0;
+// for (i; i <= 10; i++) {
+//     console.log("Se esta ejecutando un for hasta el " + i)
 // }
 
-// condiciones pueden dar como resultado un true o un false
 
-// if (true) // SI condicion == true, entonces se ejecuta {}
-//     {
-//         console.log("Condicion cumplida o verdadera")
+// let ingreseUnNumero = parseInt(prompt("Ingrese un numero"));
+// console.log("Tabla de Multiplicar del " + ingreseUnNumero);
+
+// for (let i = 1; i <= 10; i++) {
+//     let r = ingreseUnNumero * i; // r = 3 * 1
+//     console.log(ingreseUnNumero + " x " + i + " = " + r)
+// }
+
+
+// let nombre = prompt("Ingrese su Nombre")
+
+// for(let i = 1; i <= 5; i++ ) {
+//     console.log("Su nombre es: " + nombre);
+// }
+
+// for (let i = 1; i <= 100; i++) {
+//     console.log("Los numeros son: " + i);
+//     if (i == 19) {
+//         console.log("Esto esta dentro del IF: " + i);
+//         break; // Corta la ejecucion del for
+//     }    
+// }
+
+// for (let i = 1; i <= 20; i++) {
+
+//     if (i == 10) {
+//         continue; // Saltea el 10
 //     }
+//     console.log("Los numeros son: " + i);
+// }
 
-// if (false) // condicion == false por eso no se ejecuta
-//     {
-//         console.log("Condicion No cumplida o falsa | No se imprime")
+/**
+ *  i % 2 === 0 // Numeros Pares
+ *  i % 2 !== 0 // Numeros Impares
+ */
+
+
+// for (let i = 1; i <= 20; i++) {
+
+//     if (i % 2 !== 0) {
+//         continue; // Saltea los numeros Impares
 //     }
-
-// // Las Booleanas tambien son variables primitivas
-// const V = true;
-// const F = false;
-
-// if (V) { // El Valor de verdad de la variable es true
-//     console.log("Condicion cumplida o verdadera usando V")
+//     console.log("Los numeros Pares son: " + i);
 // }
 
-// dentro de los () debemos poner una condicion
-// Ejemplo:
+// Ciclo While
+// let ingreseUnValor = prompt("Ingrese un numero");
 
-// const EDAD = 18;
-
-// const edadIngresada = prompt("Ingrese su Edad: ");
-
-// if (edadIngresada >= EDAD) { // Estamos haciendo una comparacion
-//     alert("Puede ingresar.!")
-// } else { // Sino ejecuta esto:
-//     alert("Es menor a " + EDAD + " no puede pasar.!");
+// while(ingreseUnValor != 0) {
+//     console.log("El usuario ingreso: " + ingreseUnValor);
+//     ingreseUnValor = prompt("Ingrese otro valor"); // Poner el freno de mano
 // }
 
-// Comparaciones
-// Ejemplo con strings
+// Ciclo do While
+// let ingreseUnValor = prompt("Ingrese un numero");
 
-// let color = "Azul";
-// let ingreseUnColor = prompt("Ingrese un color:")
+// do {
+//     console.log("El usuario ingreso: " + ingreseUnValor);
+//     ingreseUnValor = prompt("Ingrese otro valor");
+// } while (ingreseUnValor != 0)
 
-// if (ingreseUnColor == color) {
-//     console.log("El color ingresado es " + color)
-// } else {
-//     console.warn("El color ingresado NO ES " + color + " Usted ingreso: " + ingreseUnColor)
-// }
+//  Switch
 
-// const PI = 3.14;
-// const ingreseUnValor = prompt("Ingrese un número:")
+let ingreseSuNombre = prompt("Ingrese su Nombre");
 
-// if (ingreseUnValor <= 0) {
-//     console.warn("No se puede ingresar un Valor Negativo o Igual a 0")
-// } else if (ingreseUnValor < PI) {
-//     console.log("El numero ingresado es menor a PI")
-// } else if (ingreseUnValor == PI) {
-//     console.log("El numero ingresado es igual a PI")
-// } else if (ingreseUnValor > PI) {
-//     console.log("El numero ingresado es mayor a PI")
-// } else {
-//     console.error("El dato ingresado no es un Número.!")
-// }
-
-// Tabla de Verdad (logica proposiocional)
-
-// const V = true;
-// const F = false;
-
-// console.log("Tabla de Verdad del Operador && (Y)");
-// console.log(V && V); // Si ambos son V => true
-// console.log(V && F); // Si al menos uno de los valores es F => false
-// console.log(F && V); // Si al menos uno de los valores es F => false
-// console.log(F && F); // Si ambos de los valores son F => false
-
-
-// console.log("Tabla de Verdad del Operador || (O)");
-// console.log(V || V); // Si ambos son V => true
-// console.log(V || F); // Si al menos uno de los valores es V => true
-// console.log(F || V); // Si al menos uno de los valores es V => true
-// console.log(F || F); // Si ambos de los valores son F => false
-
-// console.log("Tabla de Verdad del Operador de Negación (!)");
-// console.log(!V); // Si no es Verdadero => false
-// console.log(!F); // Si no es False => true
-
-
-// const VACIO = "";
-// const ingreseUnNumeroPorPrompt = prompt("Ingrese un numero")
-
-// if (ingreseUnNumeroPorPrompt == VACIO) {
-//     console.warn("No ha ingresado nada")
-// } else if (ingreseUnNumeroPorPrompt < 0 || ingreseUnNumeroPorPrompt == 0) {
-//     console.warn("No se puede ingresar un Valor Negativo o Igual a 0")
-// } else if (ingreseUnNumeroPorPrompt > 0 && ingreseUnNumeroPorPrompt < 1) {
-//     console.log("El numero ingresado esta entre 0 y 1")
-// } else if (ingreseUnNumeroPorPrompt == 1) {
-//     console.log(" Usted ingreso el 1")
-// } else if (ingreseUnNumeroPorPrompt > 1 && (ingreseUnNumeroPorPrompt < 50 || ingreseUnNumeroPorPrompt == 50 )) { 
-// // (ingreseUnNumeroPorPrompt < 50 || ingreseUnNumeroPorPrompt == 50 ) es lo mismo que poner ingreseUnNumeroPorPrompt <= 50
-//     console.log("El numero ingresado esta entre 1 y 50")
-// } else if (ingreseUnNumeroPorPrompt > 50) {
-//     console.log("El numero ingresado es mayor a 50")
-// } else {
-//     console.error("El dato ingresado no es un Número.!")
-// }
-
-
-// Actividad en Clase:
-// if (prompt("Ingrese un Valor: ") > 1000) {
-//     alert("Numero Mayor a 1000")
-// }
-
-// if(prompt("Ingrese un Texto") == "Hola") {
-//     console.warn("Hola")
-// }
-
-// const valor = prompt("Ingrese un Valor");
-
-// if ((valor > 10 || valor == 10 ) && ( valor < 50 || valor == 50)) {
-//     alert("Aprobo.!!")
-// }
+while (ingreseSuNombre != "Chau") {
+    switch (ingreseSuNombre) {
+        case "Alejandro":
+            console.log("Su nombre es Alejandro");
+            break;
+        case "JavaScript":
+            console.log(ingreseSuNombre + " No es un Nombre");
+            break;
+        default:
+            console.log("Nos vemos en Disney");
+            break;
+    }
+    ingreseSuNombre = prompt("Ingrese otro Nombre");
+}
