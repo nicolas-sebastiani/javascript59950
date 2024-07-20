5d79d48ad05402b35eef1abd73602558cabf6cb9
nicolas <nicosebastiani90@gmail.com>
2024-07-19 21:23:41 -0300

    ejercisios hasta 20

--- javascript---59950-main/js/ejercisios_03.js
+++ javascript---59950-main/js/ejercisios_03.js
@@ -29,7 +29,7 @@
 //     console.log(paises[i])
 // }
 
-// 6 a
+// 6 a 10
 
 // let i = 0
 // while (i < 10) {
@@ -37,10 +37,107 @@
 //     console.log(i)
 // }
 
-let i = 0;
-let suma = 0;
-let resultado = 0;
-while (i < 50){
-    i++
-    resultado += i 
-}
+// let i = 0;
+// let resultado = 0;
+// while (i < 50){
+//     i++
+//     resultado += i 
+// }
+// console.log(resultado)
+
+// let i = 0
+// while (i < 20) {
+//     i++
+//     console.log(i)
+// }
+
+// let userNumber = 1;
+// while (userNumber > 0) {
+//     userNumber = parseInt(prompt('Insert a numer: '))
+// }
+
+// let contador = 0;
+// let userPrompt = 'hola';
+// while (userPrompt == 'hola') {
+//     userPrompt = prompt('Escriba la palabra hola: ').toLowerCase()
+//     if (userPrompt == 'hola'){
+//         contador++
+//     }
+// }
+// console.log(contador);
+
+// de 11 a 15
+
+// let i = 1
+// do {
+//     console.log(i)
+//     i++
+// } while (i <= 10)
+
+// let i = 0
+// let resultado = 0
+// do {
+//     i++
+//     resultado += i
+// } while (i <= 30)
+// console.log(resultado)
+
+// let i = 0
+// do {
+//     i += 3
+//     console.log(i)
+// } while (i <= 29)
+
+// let userNumber = 0
+// do {
+//     userNumber = parseInt(prompt('Insert a number: '))
+// } while (userNumber != 0)
+
+// let secretNumber = 5
+// let userNumber = 0
+// let counter = 0
+// do {
+//     userNumber = parseInt(prompt('Adivina el numero secreto: '))
+//     counter ++
+// } while (userNumber !== secretNumber)
+// console.log(`El usuario intento ${counter} veces hasta adivinar el numero ${secretNumber}.`)
+
+// 16 a 20
+
+// let arrayNumbers = [2, 10, 20, 25, 33, 48, 51, 60, 100];
+// for (i = 0; i >= arrayNumbers.length; i++) {
+//     if (arrayNumbers[i] > 50){
+//         break
+//     }
+// }
+// console.log('Se encontro un numero mayor a 50 en el array.')
+
+// let i = 0
+// while (i <= 15) {
+//     i++
+//     if (i === 11 ){
+//         break
+//     }
+// }
+
+// let userNumber = 0
+// do {
+//     userNumber = parseInt(prompt('Ingrese un numero: '))
+// } while (userNumber > 0 )
+
+// for (i = 1; i <= 100; i++) {
+//     console.log(i)
+//     if (i % 37 == 0){
+//         break
+//     }
+// }
+
+// for (i = 1; i <= 20; i++) {
+//     console.log(i)
+//     if (i === 13){
+//         break
+//     }
+// }
+
+// 21 a 
+
