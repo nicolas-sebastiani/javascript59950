197bd5d3a19dc466724f2d2de49ffc197e408ec0
nicolas <nicosebastiani90@gmail.com>
2024-07-26 19:39:22 -0300

    ejercicios clase 04 hasta 11

--- javascript---59950-main/js/ejercicios_04.js
+++ javascript---59950-main/js/ejercicios_04.js
@@ -12,10 +12,16 @@
 // }
 // console.log(sumar(2,3))
 
-// 6 a 
+// 6 a 11
 
 // function multiplicar (n1,n2,n3){
 //     return n1 * n2 * n3
 // }
 // console.log(multiplicar(2,2,3))
+
+// const Cuadrado = function(n1) {return n1 * n1}
+// console.log(Cuadrado(8))
+
+// const esPar = (n1) => {if(n1 % 2 === 0){true}}
+// console.log(esPar(2))
 
