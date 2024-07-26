e4bb515d5f4610b05e8d4579c610e8cc0bcf1fcf
nicolas <nicosebastiani90@gmail.com>
2024-07-26 19:24:19 -0300

    first commit clase-05

--- javascript---59950-main/js/index.js
+++ javascript---59950-main/js/index.js
@@ -1,102 +1,187 @@
-// Ciclos e iteraciones
-// Actualizacion:
-// let i = 0;
-// console.log(i)
-// i = i + 1;
-// console.log(i)
-// i = i + 1;
-// console.log(i)
-// i = i + 1; // i++
-// console.log(i)
-// i++
-// console.log("i Actualizado con i++ ", i)
+// Objetos
 
-// Ciclo for
-// for(desde, hasta, actualizacion ) {
-//     lo que quiero que se ejecute;
+// let nombreProducto = "Azucar";
+// let precioProducto = 1000;
+// let categoriaProducto = "Almacen";
+// console.log(nombreProducto);
+// console.log(precioProducto);
+// console.log(categoriaProducto);
+
+// let nombreProducto1 = "Yerba";
+// let precioProducto1 = 1200;
+// let categoriaProducto1 = "Almacen";
+// console.log(nombreProducto1, precioProducto1, categoriaProducto1)
+
+
+// Definir Objeto Producto
+// const producto1 = {
+//     nombre: "Azucar",
+//     precio: 1000,
+//     categoria: "Almacen"
+// };
+// console.log(producto1);
+// console.log(producto1.nombre);
+// console.log(producto1.precio);
+// console.log(producto1.categoria);
+
+// const producto2 = {
+//     nombre: "Yerba",
+//     precio: 1200,
+//     categoria: "Almacen"
+// };
+// console.table(producto2);
+// console.log(producto2["nombre"]);
+// console.log(producto2["precio"]);
+// console.log(producto2["categoria"]);
+
+// producto1.categoria = "Dulces"; // Reasignacion de Valor
+// producto1.precio = 1200;
+
+// Constructores
+
+// function Producto (nombre, precio, categoria) {
+//     this.nombre = nombre;
+//     this.precio = precio;
+//     this.categoria = categoria;
 // }
-// let i = 0;
-// for (i; i <= 10; i++) {
-//     console.log("Se esta ejecutando un for hasta el " + i)
+
+// const producto3 = new Producto("Azucar", 1000, "Almacen");
+// console.log(producto3);
+// const producto4 = new Producto("Yerba", 1200, "Almacen");
+// console.log(producto4);
+
+// function Producto1 (objeto) {
+//     this.nombre = objeto.nombre;
+//     this.precio = objeto.precio;
+//     this.categoria = objeto.categoria;
 // }
 
+// const producto5 = new Producto1({
+//     nombre: "Yerba",
+//     precio: 1200,
+//     categoria: "Almacen"
+// })
 
-// let ingreseUnNumero = parseInt(prompt("Ingrese un numero"));
-// console.log("Tabla de Multiplicar del " + ingreseUnNumero);
+// console.log(producto5);
 
-// for (let i = 1; i <= 10; i++) {
-//     let r = ingreseUnNumero * i; // r = 3 * 1
-//     console.log(ingreseUnNumero + " x " + i + " = " + r)
+// Crear un Auto
+// function Auto(marca, color, precio, modelo, cilindraje) {
+//     this.marca = marca;
+//     this.color = color;
+//     this.precio = precio;
+//     this.modelo = modelo;
+//     this.cilindraje = cilindraje;
 // }
 
+// const auto1 = new Auto("Ford", "Azul", 100000, 2024, "2.0");
+// const auto2 = new Auto("Toyota", undefined, null, 2024, "");
+// console.log(auto1);
+// console.log(auto2);
 
-// let nombre = prompt("Ingrese su Nombre")
-
-// for(let i = 1; i <= 5; i++ ) {
-//     console.log("Su nombre es: " + nombre);
-// }
-
-// for (let i = 1; i <= 100; i++) {
-//     console.log("Los numeros son: " + i);
-//     if (i == 19) {
-//         console.log("Esto esta dentro del IF: " + i);
-//         break; // Corta la ejecucion del for
-//     }    
-// }
-
-// for (let i = 1; i <= 20; i++) {
-
-//     if (i == 10) {
-//         continue; // Saltea el 10
+// Diferencia entre Funciones y Metodos
+// function Auto(marca, color, precio, modelo, cilindraje) {
+//     this.marca = marca;
+//     this.color = color;
+//     this.precio = precio;
+//     this.modelo = modelo;
+//     this.cilindraje = cilindraje;
+//     this.encender = function () {
+//         console.log("El Auto esta encendido.!")
 //     }
-//     console.log("Los numeros son: " + i);
-// }
-
-/**
- *  i % 2 === 0 // Numeros Pares
- *  i % 2 !== 0 // Numeros Impares
- */
-
-
-// for (let i = 1; i <= 20; i++) {
-
-//     if (i % 2 !== 0) {
-//         continue; // Saltea los numeros Impares
+//     this.apagar = function () {
+//         console.log("El Auto esta apagado.!")
 //     }
-//     console.log("Los numeros Pares son: " + i);
+// }
+// const auto3 = new Auto("Ford", "Azul", 100000, 2024, "3.0");
+// console.log(auto3);
+// auto3.encender();
+// auto3.apagar();
+
+// function Moto(marca, color, precio, modelo, cilindraje) {
+//     this.marca = marca;
+//     this.color = color;
+//     this.precio = precio;
+//     this.modelo = modelo;
+//     this.cilindraje = cilindraje;
+//     this.encender = function () {
+//         console.log("La Moto esta encendida.!")
+//     }
+//     this.apagar = function () {
+//         console.log("La Moto esta apagada.!")
+//     }
 // }
 
-// Ciclo While
-// let ingreseUnValor = prompt("Ingrese un numero");
+// const moto1 = new Moto("Toyota", "Verde", 12000, 2022, "1.0");
+// console.log(moto1)
+// moto1.encender();
+// moto1.apagar();
 
-// while(ingreseUnValor != 0) {
-//     console.log("El usuario ingreso: " + ingreseUnValor);
-//     ingreseUnValor = prompt("Ingrese otro valor"); // Poner el freno de mano
+
+// const IVA = 1.21;
+// function Producto (nombre, precio, categoria) {
+//     this.nombre = nombre;
+//     this.precio = precio;
+//     this.categoria = categoria;
+
+//     this.precioFinal = function () {
+//         this.precio = precio * IVA;        
+//     } 
 // }
 
-// Ciclo do While
-// let ingreseUnValor = prompt("Ingrese un numero");
+// const producto10 = new Producto("Azucar", 1000, "Almacen");
+// console.log(producto10);
+// producto10.precioFinal();
 
-// do {
-//     console.log("El usuario ingreso: " + ingreseUnValor);
-//     ingreseUnValor = prompt("Ingrese otro valor");
-// } while (ingreseUnValor != 0)
 
-//  Switch
+// function Auto(marca, color, precio, modelo, cilindraje) {
+//     this.marca = marca;
+//     this.color = color;
+//     this.precio = precio;
+//     this.modelo = modelo;
+//     this.cilindraje = cilindraje;
+//     this.encender = function () {
+//         console.log("El Auto esta encendido.!")
+//     }
+//     this.apagar = function () {
+//         console.log("El Auto esta apagado.!")
+//     }
+// }
+// const auto3 = new Auto("Ford", "Azul", 100000, 2024, "3.0");
+// console.log(auto3);
+// auto3.encender();
+// auto3.apagar();
 
-let ingreseSuNombre = prompt("Ingrese su Nombre");
+// console.log("marca" in auto3); // true
+// console.log("nombre" in auto3);  // false
 
-while (ingreseSuNombre != "Chau") {
-    switch (ingreseSuNombre) {
-        case "Alejandro":
-            console.log("Su nombre es Alejandro");
-            break;
-        case "JavaScript":
-            console.log(ingreseSuNombre + " No es un Nombre");
-            break;
-        default:
-            console.log("Nos vemos en Disney");
-            break;
+// for (const caracteristica in auto3 ) {
+//     console.log(auto3[caracteristica])
+// }
+
+
+// Clases
+class Vehiculo {
+    constructor(tipo, marca, color, precio, modelo, cilindraje) {
+        this.tipo = tipo;
+        this.marca = marca;
+        this.color = color;
+        this.precio = precio;
+        this.modelo = modelo;
+        this.cilindraje = cilindraje;
     }
-    ingreseSuNombre = prompt("Ingrese otro Nombre");
+    encender() {
+        console.log("El/La " + this.tipo + " esta encendido/a.!")
+    }
+    apagar() {
+        console.log("El/La " + this.tipo + "  esta apagado/a.!")
+    }
 }
+
+const auto = new Vehiculo("Auto", "Ford", "Azul", 100000, 2024, "3.0");
+console.log(auto);
+const moto = new Vehiculo("Moto", "Toyota", "Verde", 12000, 2022, "1.0");
+console.log(moto);
+auto.encender();
+auto.apagar();
+moto.encender();
+moto.apagar();
