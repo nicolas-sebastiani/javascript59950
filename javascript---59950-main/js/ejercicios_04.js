006a5883e991b5535b4064999534d5115eb9442b
nicolas <nicosebastiani90@gmail.com>
2024-07-26 19:21:59 -0300

    ejercicios hasta 8 clase 4

--- javascript---59950-main/js/ejercicios_04.js
+++ javascript---59950-main/js/ejercicios_04.js
@@ -5,17 +5,17 @@
 // function saludar(){
 //     console.log(mensaje)
 // }
-
 // saludar()
 
 // function sumar(n1,n2){
 //     return n1 + n2
 // }
-
 // console.log(sumar(2,3))
 
 // 6 a 
 
-function multiplicar (n1,n2,n3){
-    
-}
+// function multiplicar (n1,n2,n3){
+//     return n1 * n2 * n3
+// }
+// console.log(multiplicar(2,2,3))
+
