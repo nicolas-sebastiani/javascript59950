3e37118004afaa125e3753d7819ee7246de70def
nicolas <nicosebastiani90@gmail.com>
2024-07-26 21:00:44 -0300

    primer pre entrega lista

--- javascript---59950-main/js/1preentrega.js
+++ javascript---59950-main/js/1preentrega.js
@@ -1,7 +1,7 @@
 alert("CHAPAS METALICAS SRL")
 alert("Calculadora de precio de chapas de metal de 1mt x 1.5mts")
 
-const precioDolar = 1500
+const precioDolar = 1480
 const densidadAluminio = 2.72
 const densidadAceroalCarbono = 7.84
 const densidadInoxidable = 8.03
@@ -10,84 +10,102 @@ let espesor = 0
 let cantidad = 0
 let precioFinal = 0
 
+function precioEnDolar (precioFinal) {
+    let dolares = (precioFinal / precioDolar).toFixed(2)
+    alert(`El precio en Dolares sera de $${dolares}`)
+}
+
 while (material != 4){
     material = prompt('| 1 - Aluminio | 2 - Acero al Carbono | 3 - Acero Inoxidable | 4 - Salir | Seleccione un Material: ');
     if (material == 1){
-        espesor = parseFloat(prompt(' Seleccione un espesor (mm): |  1.6  |  2  |  3.2  |  4.7  |  6.35  |'));
+        espesor = prompt(' Seleccione un espesor (mm): |  1.6  |  2  |  3.2  |  4.7  |  6.35  |');
         cantidad = parseInt(prompt('Ingrese cantidad de chapa a comprar: '));
         switch(espesor){
-            case 1.6:
-                precioFinal = (1.6 *( 150 * 100 )* densidadAluminio) * cantidad;
+            case '1.6':
+                precioFinal = ((1.6 *( 150 * 100 )* densidadAluminio) * cantidad).toFixed(2);
                 alert(`El precio de ${cantidad} chapas de Aluminio sera de $${precioFinal}`);
+                precioEnDolar(precioFinal.toFixed(2));
                 break;
-            case 2:
-                precioFinal = (2 *( 150 * 100 )* densidadAluminio) * cantidad;
+            case '2':
+                precioFinal = ((2 *( 150 * 100 )* densidadAluminio) * cantidad).toFixed(2);
                 alert(`El precio de ${cantidad} chapas de Aluminio sera de $${precioFinal}`);
+                precioEnDolar(precioFinal.toFixed(2));
                 break;
-            case 3.2:
-                precioFinal = (3.2 *( 150 * 100 )* densidadAluminio) * cantidad;
+            case '3.2':
+                precioFinal = ((3.2 *( 150 * 100 )* densidadAluminio) * cantidad).toFixed(2);
                 alert(`El precio de ${cantidad} chapas de Aluminio sera de $${precioFinal}`);
+                precioEnDolar(precioFinal.toFixed(2));
                 break;
-            case 4.7:
-                precioFinal = (4.7 *( 150 * 100 )* densidadAluminio) * cantidad;
+            case '4.7':
+                precioFinal = ((4.7 *( 150 * 100 )* densidadAluminio) * cantidad).toFixed(2);
                 alert(`El precio de ${cantidad} chapas de Aluminio sera de $${precioFinal}`);
+                precioEnDolar(precioFinal.toFixed(2));
                 break;
-            case 6.35:
-                precioFinal = (6.35 *( 150 * 100 )* densidadAluminio) * cantidad;
+            case '6.35':
+                precioFinal = ((6.35 *( 150 * 100 )* densidadAluminio) * cantidad).toFixed(2);
                 alert(`El precio de ${cantidad} chapas de Aluminio sera de $${precioFinal}`);
+                precioEnDolar(precioFinal.toFixed(2));
                 break;
             }
     } else if (material == 2) {
-        espesor = parseFloat(prompt('| 1.6 | 2 | 3.2 | 4.7 | 6.35 | Seleccione un espesor: '));
+        espesor = prompt('| 1.6 | 2 | 3.2 | 4.7 | 6.35 | Seleccione un espesor: ');
         cantidad = parseInt(prompt('Ingrese cantidad de chapa a comprar: '));
         switch(espesor){
-            case 1.6:
-                precioFinal = (1.6 *( 150 * 100 )* densidadAceroalCarbono) * cantidad;
+            case '1.6':
+                precioFinal = ((1.6 *( 150 * 100 )* densidadAceroalCarbono) * cantidad).toFixed(2);
                 alert(`El precio de ${cantidad} chapas de Aluminio sera de $${precioFinal}`);
+                precioEnDolar(precioFinal.toFixed(2));
                 break;
-            case 2:
-                precioFinal = (2 *( 150 * 100 )* densidadAceroalCarbono) * cantidad;
+            case '2':
+                precioFinal = ((2 *( 150 * 100 )* densidadAceroalCarbono) * cantidad).toFixed(2);
                 alert(`El precio de ${cantidad} chapas de Aluminio sera de $${precioFinal}`);
+                precioEnDolar(precioFinal.toFixed(2));
                 break;
-            case 3.2:
-                precioFinal = (3.2 *( 150 * 100 )* densidadAceroalCarbono) * cantidad;
+            case '3.2':
+                precioFinal = ((3.2 *( 150 * 100 )* densidadAceroalCarbono) * cantidad).toFixed(2);
                 alert(`El precio de ${cantidad} chapas de Aluminio sera de $${precioFinal}`);
+                precioEnDolar(precioFinal.toFixed(2));
                 break;
-            case 4.7:
-                precioFinal = (4.7 *( 150 * 100 )* densidadAceroalCarbono) * cantidad;
+            case '4.7':
+                precioFinal = ((4.7 *( 150 * 100 )* densidadAceroalCarbono) * cantidad).toFixed(2);
                 alert(`El precio de ${cantidad} chapas de Aluminio sera de $${precioFinal}`);
+                precioEnDolar(precioFinal.toFixed(2));
                 break;
-            case 6.35:
-                precioFinal = (6.35 *( 150 * 100 )* densidadAceroalCarbono) * cantidad;
+            case '6.35':
+                precioFinal = ((6.35 *( 150 * 100 )* densidadAceroalCarbono) * cantidad).toFixed(2);
                 alert(`El precio de ${cantidad} chapas de Aluminio sera de $${precioFinal}`);
+                precioEnDolar(precioFinal.toFixed(2));
                 break;
             }
     } else if (material == 3) {
-        espesor = parseFloat(prompt('| 1.6 | 2 | 3.2 | 4.7 | 6.35 | Seleccione un espesor: '));
+        espesor = prompt('| 1.6 | 2 | 3.2 | 4.7 | 6.35 | Seleccione un espesor: ');
         cantidad = parseInt(prompt('Ingrese cantidad de chapa a comprar: '));
         switch(espesor){
-            case 1.6:
-                precioFinal = (1.6 *( 150 * 100 )* densidadInoxidable) * cantidad;
+            case '1.6':
+                precioFinal = ((1.6 *( 150 * 100 )* densidadInoxidable) * cantidad).toFixed(2);
                 alert(`El precio de ${cantidad} chapas de Aluminio sera de $${precioFinal}`);
+                precioEnDolar(precioFinal.toFixed(2));
                 break;
-            case 2:
-                precioFinal = (2 *( 150 * 100 )* densidadInoxidable) * cantidad;
+            case '2':
+                precioFinal = ((2 *( 150 * 100 )* densidadInoxidable) * cantidad).toFixed(2);
                 alert(`El precio de ${cantidad} chapas de Aluminio sera de $${precioFinal}`);
+                precioEnDolar(precioFinal.toFixed(2));
                 break;
-            case 3.2:
-                precioFinal = (3.2 *( 150 * 100 )* densidadInoxidable) * cantidad;
+            case '3.2':
+                precioFinal = ((3.2 *( 150 * 100 )* densidadInoxidable) * cantidad).toFixed(2);
                 alert(`El precio de ${cantidad} chapas de Aluminio sera de $${precioFinal}`);
+                precioEnDolar(precioFinal.toFixed(2));
                 break;
-            case 4.7:
-                precioFinal = (4.7 *( 150 * 100 )* densidadInoxidable) * cantidad;
+            case '4.7':
+                precioFinal = ((4.7 *( 150 * 100 )* densidadInoxidable) * cantidad).toFixed(2);
                 alert(`El precio de ${cantidad} chapas de Aluminio sera de $${precioFinal}`);
+                precioEnDolar(precioFinal.toFixed(2));
                 break;
-            case 6.35:
-                precioFinal = (6.35 *( 150 * 100 )* densidadInoxidable) * cantidad;
+            case '6.35':
+                precioFinal = ((6.35 *( 150 * 100 )* densidadInoxidable) * cantidad).toFixed(2);
                 alert(`El precio de ${cantidad} chapas de Aluminio sera de $${precioFinal}`);
+                precioEnDolar(precioFinal);
                 break;
             }
-    } else {
-        material = prompt('| 1 - Aluminio | 2 - Acero al Carbono | 3 - Acero Inoxidable | 4 - Salir | Seleccione un Material: ');
-    }
+    } 
 }
