9af3a1f4762c5b1c61a26324648bf6a4cee73e7b
nicolas <nicosebastiani90@gmail.com>
2024-08-01 22:53:50 -0300

    ejercicios hasta el 6 clase06

--- javascript---59950-main/js/ejercicios_06.js
+++ javascript---59950-main/js/ejercicios_06.js
@@ -1,1 +1,16 @@
 // 1 a 
+
+const personas = [
+    {nombre : 'nico', edad : 33},
+    {nombre : 'ramon', edad : 15},
+    {nombre : 'pedro', edad : 20},
+]
+console.log(personas)
+personas.push({nombre: 'omar', edad: 54})
+console.log(personas)
+personas.shift()
+console.log(personas)
+const mayorDe30 = personas.filter((elemento) => elemento.edad > 30);
+console.log(mayorDe30)
+console.log(personas.sort())
+
