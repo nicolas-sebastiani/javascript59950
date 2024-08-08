4dcb1434364cc655f43a6dc2d5a4f8038465990f
nicolas <nicosebastiani90@gmail.com>
2024-08-08 17:53:06 -0300

    tercer comit clase7

--- javascript---59950-main/js/ejercicios_07.js
+++ javascript---59950-main/js/ejercicios_07.js
@@ -15,3 +15,29 @@ const numeros = [3 ,6 , 35, 1, 22, 19, 13, 59, 93, 60, 10]
 // const duplicados = numeros.map(function (numero) {return numero * 2;})
 // console.log(duplicados)
 
+// sumaNumeros = numeros.reduce((total, numero) => total + numero, 0)
+// console.log(sumaNumeros)
+
+// console.log(palabras.sort())
+
+// class Alumnos {
+//     constructor(nombre, edad, notas){
+//         this.nombre = nombre;
+//         this.edad = edad;
+//         this.notas = notas;
+//     }
+// }
+
+// const alumno1 = new Alumnos('nicolas', 33, [9, 7, 10])
+// const alumno2 = new Alumnos('barbara', 33, [10, 9, 8])
+// const alumno3 = new Alumnos('pedro', 33, [5, 3, 1])
+
+// sumatoria = alumno2.notas.reduce((total, numero) => total + numero, 0)
+// promedio = sumatoria / 3
+// console.log(promedio)
+
+// const tempCelc = [5, 8, 12, 32, 35];
+// console.log(tempCelc)
+// const tempFaren = tempCelc.map((temperatura) => temperatura * 1.8 + 32)
+// console.log(tempFaren)
+
